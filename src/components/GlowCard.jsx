34dd973131a69298ce402aa9f5e53b2fc321648e
eliseucostaalbano <ELISEUALBANO57@GMAIL.COM>
2025-11-10 13:18:47 -0300

    terminado o componente glowCard

--- src/components/GlowCard.jsx
+++ src/components/GlowCard.jsx
@@ -1,20 +1,43 @@
-const GlowCard = ({ card , children  }) => {
+import { useRef } from "react";
+
+const GlowCard = ({ card, index, children }) => {
+  const cardRefs = useRef([]);
+
+  const lidarMoveMouse = (index) => (e) => {
+    const card = cardRefs.current[index];
+    if (!card) return;
+
+    const rect = card.getBoundingClientRect();
+    const mouseX = e.clientX - rect.left - rect.width / 2;
+    const mouseY = e.clientY - rect.top - rect.width / 2;
+
+     let angulo = Math.atan2(mouseY, mouseX) * (180 / Math.PI);
+     
+     angulo = (angulo + 360) % 360;
+
+     card.style.setProperty("--start", angulo + 60);
+  };
+
   return (
-    <div className="card card-border timeline-card rounded-xl p-10 mb-5 break-inside-avoid-column">
+    <div
+      ref={(el) => (cardRefs.current[index] = el)}
+      onMouseMove={lidarMoveMouse(index)}
+      className="card card-border timeline-card rounded-xl p-10 mb-5 break-inside-avoid-column"
+    >
       <div className="glow"></div>
       <div className="flex items-center gap-1 mb-5">
-       {Array.from({ length: card.nota }, (_, i) => (
+        {Array.from({ length: card.nota }, (_, i) => (
           <img key={i} src="/images/star.png" alt="star" className="size-5" />
-        ))} 
+        ))}
       </div>
       <div>
         <div className="mb-5">
-        <p className="text-white-50 text-lg">{card.review}</p>
-      </div>
-      {children}
+          <p className="text-white-50 text-lg">{card.review}</p>
+        </div>
+        {children}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default GlowCard
+export default GlowCard;
