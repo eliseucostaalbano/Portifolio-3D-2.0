1fb6dfa6d5fbd97eab348ce19601b83adbba6cb7
eliseucostaalbano <ELISEUALBANO57@GMAIL.COM>
2025-11-10 13:56:48 -0300

    terminado o componente experiencia

--- src/sections/Experiencia.jsx
+++ src/sections/Experiencia.jsx
@@ -1,31 +1,115 @@
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
 import GlowCard from "../components/GlowCard.jsx";
-import Titulo from "../components/Titulo"
+import Titulo from "../components/Titulo";
 import { expCards } from "../constants";
 
+gsap.registerPlugin(ScrollTrigger);
 
 const Experiencia = () => {
-  return (
-    <section id="experiencia"  className="flex-center md:mt-40 mt-20 section-padding xl:px-0">
-     <div className="w-full h-full md:px-20 px-5">
-    <Titulo titulo="Meu percurso profissional" sub="💼 Minha Carreira - Visão geral" />
-    <div className="mt-32 relative">
-    <div className="relative z-50 xl:space-y-32 space-y-10">
-      {expCards.map((card, index) => (
-        <div key={index} className="exp-card-wrapper">
-          <div className="xl:w-2/6">
-         <GlowCard card={card}>
-         <div>
-          <img src={card.imgPath} alt="exp-img" />
-         </div>
-         </GlowCard>
-         </div>
-         </div>
-      ))}
-    </div>
-    </div>
-     </div>
-    </section>
-  )
-}
+  useGSAP(() => {
+    gsap.utils.toArray(".timeline-card").forEach((card) => {
+      gsap.from(card, {
+        xPercent: -100,
+        opacity: 0,
+        transformOrigin: "left left",
+        duration: 1,
+        ease: "power2.inOut",
+        scrollTrigger: {
+          trigger: card,
 
-export default Experiencia
+          start: "top 80%",
+        },
+      });
+    });
+
+    gsap.to(".timeline", {
+     transformOrigin: "bottom bottom",
+      ease: "power1.inOut",
+        scrollTrigger: {
+        trigger: ".timeline",
+        start: "top center",
+        end: "70% center",
+         onUpdate: (self) => {
+          gsap.to(".timeline", {
+            scaleY: 1 - self.progress,
+          });
+        },
+      },
+    })
+
+    gsap.utils.toArray(".expText").forEach((text) => {
+      gsap.from(text, {
+        xPercent: 0,
+        opacity: 0,
+        duration: 1,
+        ease: "power2.inOut",
+        scrollTrigger: {
+          trigger: text,
+          start: "top 60%",
+        },
+
+      });
+    });
+  }, []);
+
+  return (
+    <section id="experiencia"className="flex-center md:mt-40 mt-20 section-padding xl:px-0">
+      <div className="w-full h-full md:px-20 px-5">
+        <Titulo titulo="Meu percurso profissional" sub="💼 Minha Carreira - Visão geral"/>
+        <div className="mt-32 relative">
+          <div className="relative z-50 xl:space-y-32 space-y-10">
+            {expCards.map((card, index) => (
+              <div key={index} className="exp-card-wrapper">
+                <div className="xl:w-2/6">
+                  <GlowCard card={card}>
+                    <div>
+                      <img src={card.imgPath} alt="exp-img" />
+                    </div>
+                  </GlowCard>
+                </div>
+                <div className="xl:w-4/6">
+                  <div className="flex items-start">
+                    <div className="timeline-wrapper">
+                      <div className="timeline" />
+                      <div className="gradient-line w-1 h-full" />
+                    </div>
+                    <div className="expText flex xl:gap-20 md:gap-10 gap-5 relative z-20">
+                      <div className="timeline-logo">
+                        <img src={card.logoPath} alt="logo" />
+                      </div>
+                      <div>
+                        <h1 className="font-semibold text-3xl">
+                          {card.titulo}
+                        </h1>
+                        <p className="my-5 text-white-50">
+                          🗓️&nbsp;{card.data}
+                        </p>
+                        <p className="text-[#839CB5] italic">
+                          Responsibilidades
+                        </p>
+                        <ul className="list-disc ms-5 mt-5 flex flex-col gap-5 text-white-50">
+                          {card.responsabilidades.map(
+                            (responsabilidade, index) => (
+                              <li key={index} className="text-lg">
+                                {responsabilidade}
+                              </li>
+                            )
+                          )}
+                        </ul>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Experiencia;
